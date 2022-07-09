91ec7b70a3f92de18992777b76312dfe2aefed33
YukiChiyoda <me@snowx.cc>
2022-07-09 11:00:25 +0800

    Update: NProgress

--- jar/src/main.ts
+++ jar/src/main.ts
@@ -1,8 +1,28 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import ElementPlus from 'element-plus'
-// import 'element-plus/dist/index.css'
-import './styles/element/index.scss'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import ElementPlus from 'element-plus';
+import NProgress from 'nprogress';
+// import 'element-plus/dist/index.css';
+import './styles/public/element.scss';
+import 'nprogress/nprogress.css'
+import './styles/public/nprogress.scss';
 
-createApp(App).use(router).use(ElementPlus).mount('#app')
+createApp(App).use(router).use(ElementPlus).mount('#app');
+
+NProgress.configure({
+    easing: 'ease',
+    speed: 500,
+    trickleSpeed: 200,
+    minimum: 0.3,
+    showSpinner: false
+});
+
+router.beforeEach((target, origin, next) => {
+    NProgress.start();
+    next();
+});
+
+router.afterEach(() => {
+    NProgress.done();
+});
