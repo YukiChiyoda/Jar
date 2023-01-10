105ed84b4aa0b336c58b6ed81d287328f8fafaf3
YukiChiyoda <me@snowx.cc>
2023-01-10 17:05:48 +0800

    Update: ViewPage

--- jar/src/views/ViewPage.vue
+++ jar/src/views/ViewPage.vue
@@ -24,15 +24,134 @@
             <el-aside width="300px">
                 Folder Demo
             </el-aside>
-            <el-main>
-                Main Demo
-            </el-main>
+
+            <el-container>
+
+                <el-main>
+                    <div class="box">
+                        <div class="headimg">
+                            <img src="./../assets/demo.png" alt="TestImg">
+                        </div>
+                        <div class="title">
+                            Test Page
+                        </div>
+                        <div class="subtitle">
+                            This is a test page for testing.
+                        </div>
+                        <div class="detail">
+                            <el-icon color="#F8DADE">
+                                <User />
+                            </el-icon>: Yuki,
+                            <el-icon color="#F8DADE">
+                                <Mug />
+                            </el-icon>: 2023-1-10
+                        </div>
+                        <div class="title_A">The A Title</div>
+                        <div class="title_AA">The AA Title</div>
+                        <div class="title_AAA">The AAA Title</div>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab, facilis quasi.
+                            Temporibus
+                            voluptate porro culpa vero optio rerum ratione minima ex qui molestias dolor non, explicabo
+                            neque
+                            itaque harum nobis!<br />Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                            Voluptatem
+                            enim
+                            quod quae. Alias, fugit ratione fugiat illum adipisci officiis doloremque vitae harum rerum
+                            ad
+                            iure
+                            numquam, placeat laboriosam, nobis architecto!<br />Lorem ipsum dolor sit, amet consectetur
+                            adipisicing elit. Soluta pariatur non alias dolor, at commodi possimus, suscipit a obcaecati
+                            distinctio assumenda sunt deserunt harum culpa nesciunt. Architecto quam tempore tenetur.
+                            <br />Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab, facilis quasi.
+                            Temporibus
+                            voluptate porro culpa vero optio rerum ratione minima ex qui molestias dolor non, explicabo
+                            neque
+                            itaque harum nobis!<br />Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                            Voluptatem
+                            enim
+                            quod quae. Alias, fugit ratione fugiat illum adipisci officiis doloremque vitae harum rerum
+                            ad
+                            iure
+                            numquam, placeat laboriosam, nobis architecto!<br />Lorem ipsum dolor sit, amet consectetur
+                            adipisicing elit. Soluta pariatur non alias dolor, at commodi possimus, suscipit a obcaecati
+                            distinctio assumenda sunt deserunt harum culpa nesciunt. Architecto quam tempore tenetur.
+                            <br />Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab, facilis quasi.
+                            Temporibus
+                            voluptate porro culpa vero optio rerum ratione minima ex qui molestias dolor non, explicabo
+                            neque
+                            itaque harum nobis!<br />Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                            Voluptatem
+                            enim
+                            quod quae. Alias, fugit ratione fugiat illum adipisci officiis doloremque vitae harum rerum
+                            ad
+                            iure
+                            numquam, placeat laboriosam, nobis architecto!<br />Lorem ipsum dolor sit, amet consectetur
+                            adipisicing elit. Soluta pariatur non alias dolor, at commodi possimus, suscipit a obcaecati
+                            distinctio assumenda sunt deserunt harum culpa nesciunt. Architecto quam tempore tenetur.
+                            <br />Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab, facilis quasi.
+                            Temporibus
+                            voluptate porro culpa vero optio rerum ratione minima ex qui molestias dolor non, explicabo
+                            neque
+                            itaque harum nobis!<br />Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                            Voluptatem
+                            enim
+                            quod quae. Alias, fugit ratione fugiat illum adipisci officiis doloremque vitae harum rerum
+                            ad
+                            iure
+                            numquam, placeat laboriosam, nobis architecto!<br />Lorem ipsum dolor sit, amet consectetur
+                            adipisicing elit. Soluta pariatur non alias dolor, at commodi possimus, suscipit a obcaecati
+                            distinctio assumenda sunt deserunt harum culpa nesciunt. Architecto quam tempore tenetur.
+                            <br />Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab, facilis quasi.
+                            Temporibus
+                            voluptate porro culpa vero optio rerum ratione minima ex qui molestias dolor non, explicabo
+                            neque
+                            itaque harum nobis!<br />Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                            Voluptatem
+                            enim
+                            quod quae. Alias, fugit ratione fugiat illum adipisci officiis doloremque vitae harum rerum
+                            ad
+                            iure
+                            numquam, placeat laboriosam, nobis architecto!<br />Lorem ipsum dolor sit, amet consectetur
+                            adipisicing elit. Soluta pariatur non alias dolor, at commodi possimus, suscipit a obcaecati
+                            distinctio assumenda sunt deserunt harum culpa nesciunt. Architecto quam tempore tenetur.
+                            <br />Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab, facilis quasi.
+                            Temporibus
+                            voluptate porro culpa vero optio rerum ratione minima ex qui molestias dolor non, explicabo
+                            neque
+                            itaque harum nobis!<br />Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                            Voluptatem
+                            enim
+                            quod quae. Alias, fugit ratione fugiat illum adipisci officiis doloremque vitae harum rerum
+                            ad
+                            iure
+                            numquam, placeat laboriosam, nobis architecto!<br />Lorem ipsum dolor sit, amet consectetur
+                            adipisicing elit. Soluta pariatur non alias dolor, at commodi possimus, suscipit a obcaecati
+                            distinctio assumenda sunt deserunt harum culpa nesciunt. Architecto quam tempore tenetur.
+                            <br />Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab, facilis quasi.
+                            Temporibus
+                            voluptate porro culpa vero optio rerum ratione minima ex qui molestias dolor non, explicabo
+                            neque
+                            itaque harum nobis!<br />Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                            Voluptatem
+                            enim
+                            quod quae. Alias, fugit ratione fugiat illum adipisci officiis doloremque vitae harum rerum
+                            ad
+                            iure
+                            numquam, placeat laboriosam, nobis architecto!<br />Lorem ipsum dolor sit, amet consectetur
+                            adipisicing elit. Soluta pariatur non alias dolor, at commodi possimus, suscipit a obcaecati
+                            distinctio assumenda sunt deserunt harum culpa nesciunt. Architecto quam tempore tenetur.
+                        </p>
+                    </div>
+                </el-main>
+
+                <el-footer height="100px">
+                    Copyright © 2023 Yuki·Chiyoda
+                </el-footer>
+
+            </el-container>
 
         </el-container>
-
-        <el-footer height="100px">
-            Copyright © 2023 Yuki·Chiyoda
-        </el-footer>
 
     </el-container>
 </template>
